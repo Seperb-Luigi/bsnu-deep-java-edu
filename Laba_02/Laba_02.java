a8594e3268fe1cfc3120f01f5d2468381cd347bd
Viktor <vitia1009.05@gmail.com>
2024-12-08 15:32:36 +0200

    viktor

---
+++ Laba_02/Laba_02.java
@@ -0,0 +1,20 @@
+package Laba_02;
+
+public class Laba_02 {
+    public static void main(String[] args) {
+        char sunSymbol = '\u2609';
+        char chessKing = '\u2654';
+        char musicalNote = '\u266B';
+        char smilingFace = '\u263A';
+        char yinYang = '\u262F';
+
+
+
+        System.out.println("Stored Unicode Characters using Escape Sequences:");
+        System.out.println("Sun Symbol: " + sunSymbol);
+        System.out.println("Chess King: " + chessKing);
+        System.out.println("Musical Note: " + musicalNote);
+        System.out.println("Smiling Face: " + smilingFace);
+        System.out.println("Yin Yang: " + yinYang);
+    }
+}
